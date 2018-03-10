c25adcb7d7c2e1ce62bb4c08a59810f2a3346c1f
Carlos <Carlos@192.168.56.1>
2018-03-10 17:48:40 -0600

    import inicial

---
+++ ProjetcLab2/src/projetclab2/Empleado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetclab2;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Empleado {
+    private String dui;
+    private String nombres;
+    private String apellidos;
+    protected java.util.GregorianCalendar fechaIngreso;    
+    
+}
