b112a3eadd232e563ccabc1b6bec0c1b015b9bbd
Rene Gomez <Rene Gomez@DESKTOP-EF34MAL>
2018-03-10 18:21:39 -0600

    Generación de Contructor, Getters y Setters de la clase Empleado.

--- ProjetcLab2/src/projetclab2/Empleado.java
+++ ProjetcLab2/src/projetclab2/Empleado.java
@@ -5,6 +5,8 @@
  */
 package projetclab2;
 
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author Carlos
@@ -13,6 +15,44 @@ public class Empleado {
     private String dui;
     private String nombres;
     private String apellidos;
-    protected java.util.GregorianCalendar fechaIngreso;    
-    
+    protected GregorianCalendar fechaIngreso;
+
+    public Empleado(String dui, String nombres, String apellidos, GregorianCalendar fechaIngreso) {
+        this.dui = dui;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public GregorianCalendar getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(GregorianCalendar fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }    
 }
